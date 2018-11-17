6e1a58ec8f3a3829eff7a07c884162566ea914d9
patrickcunha <patrick.cunha336@gmail.com>
2018-11-16 22:41:28 -0300

    fix routes

--- routes/model.js
+++ routes/model.js
@@ -3,15 +3,11 @@ import AuthController from '../controllers/auth';
 
 //app = express
 module.exports = app => {
-    app.route('/').all((req, res) => {
-
-        res.send("teste")
-
-    })
+    app.route('/').all((req, res) => res.send("teste"))
     //middleware instancia class ModelController e atribui o objeto instanciado na requisição
     app.route('/v1/a/:collection/:id').all(async (req, res, next) => {
 
-        let authController = new AuthController(req)
+        const authController = new AuthController(req)
 
         const auth = await authController.verifyAuth()
 
@@ -21,23 +17,19 @@ module.exports = app => {
 
             next()
 
-        } else {
-
-            res.json(auth)
-
-        }
-
+        } 
+        else res.json(auth)
     })
+    
+    .get( async (req,res) => res.json( await req.modelController.selectAny() ) )
 
-        .get( async (req,res) => res.json( await req.modelController.selectAny() ) )
+    .put( async (req,res) => res.json( await req.modelController.update() ) )
 
-        .put( async (req,res) => res.json( await req.modelController.update() ) )
-
-        .delete( async (req,res) => res.json( await req.modelController.delete() ) )
+    .delete( async (req,res) => res.json( await req.modelController.delete() ) )
 
     app.route('/v1/a/:collection').all(async (req, res, next) => {
 
-        let authController = new AuthController(req)
+        const authController = new AuthController(req)
 
         const auth = await authController.verifyAuth()
 
@@ -52,8 +44,9 @@ module.exports = app => {
         else res.json(auth)
 
     })
-        .get( async (req,res) => res.json( await req.modelController.selectAll() ) )
+    
+    .get( async (req,res) => res.json( await req.modelController.selectAll() ) )
 
-        .post( async (req,res) => res.json( await req.modelController.store() ) )
+    .post( async (req,res) => res.json( await req.modelController.store() ) )
 
 }
