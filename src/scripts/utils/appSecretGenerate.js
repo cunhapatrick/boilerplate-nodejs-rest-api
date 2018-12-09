b9d3821e36421d3005ed6ec3ff10001397f0c8cc
Patrick D Paulo Baia Cunha <patrick.cunha336@gmail.com>
2018-12-09 03:48:40 -0300

    Update appSecretGenerate.js

--- src/scripts/utils/appSecretGenerate.js
+++ src/scripts/utils/appSecretGenerate.js
@@ -3,15 +3,14 @@ const bcrypt = require('bcryptjs')
 const { argv } = process
 
 if (argv.length !== 3) {
-  throw new Error('Por favor passe a senha como argumento!')
+  throw new Error('Please, input the desired password as argument!')
 }
 
 const senha = argv[2]
 const hash = bcrypt.hashSync(senha)
 
-console.log('Gerando hash...')
+console.log('Generate hash...')
 console.log(
-  'APP SECRET CRIADO!!!',
-  'Por favor cole o hash abaixo dentro do respectivo DEV_APP_SECRET || APP_SECRET(DEV = develop)'
+  'APP SECRET CREATED!!!'
 )
 console.log(hash)
